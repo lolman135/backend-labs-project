ac56dcad45f7ed65573bef74d7a68a331d8ac370
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2025-10-01 13:46:41 +0300

    Initial commit

---
+++ src/main/kotlin/labs/backendlabsproject/BackendLabsProjectApplication.kt
@@ -0,0 +1,11 @@
+package labs.backendlabsproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendLabsProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendLabsProjectApplication>(*args)
+}
---
+++ src/test/kotlin/labs/backendlabsproject/BackendLabsProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package labs.backendlabsproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendLabsProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
