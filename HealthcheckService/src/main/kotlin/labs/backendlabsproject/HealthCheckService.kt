a380c9ad188c4e3f976a8273a6a35b9e8051e264
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2025-10-06 12:27:05 +0300

    Moved healthcheck to separate dir as separate service

--- HealthcheckService/src/main/kotlin/labs/backendlabsproject/HealthCheckService.kt
+++ HealthcheckService/src/main/kotlin/labs/backendlabsproject/HealthCheckService.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BackendLabsProjectApplication
+class HealthCheckService
 
 fun main(args: Array<String>) {
-    runApplication<BackendLabsProjectApplication>(*args)
+    runApplication<HealthCheckService>(*args)
 }
