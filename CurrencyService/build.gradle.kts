951b66877df91bc652278726cdbfbe4d05f228a9
Kyrylo Kusovskyi <kkirilll2006@gmail.com>
2026-03-05 14:13:46 +0200

    Prepare configs to manual deploy

--- CurrencyService/build.gradle.kts
+++ CurrencyService/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    kotlin("kapt") version "1.9.25"
+    kotlin("kapt") version "2.0.20"
     id("org.springframework.boot") version "3.5.7"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "1.9.25"
