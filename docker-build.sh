73c29259b99811bb9edd18775e17efbcf69a527b
Kyrylo Kusovskyi <kkirilll2006@gmail.com>
2026-04-06 13:21:31 +0300

    Change compose.yaml

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 
-docker build -t my-postgres ./postgres
-docker build -t my-gateway ./ApiGatewayService
-docker build -t my-auth ./AuthService
-docker build -t my-currency ./CurrencyService
-docker build -t my-expense ./ExpenseService
-docker build -t my-healthcheck ./HealthcheckService
-docker build -t my-user ./UserService
+REGISTRY="registry.gitlab.com/kkirilll2006/devops-labs"
+
+docker buildx build --platform linux/amd64 -t $REGISTRY/postgres:latest ./postgres --push
+docker buildx build --platform linux/amd64 -t $REGISTRY/gateway:latest ./ApiGatewayService --push
+docker buildx build --platform linux/amd64 -t $REGISTRY/auth:latest ./AuthService --push
+docker buildx build --platform linux/amd64 -t $REGISTRY/currency:latest ./CurrencyService --push
+docker buildx build --platform linux/amd64 -t $REGISTRY/expense:latest ./ExpenseService --push
+docker buildx build --platform linux/amd64 -t $REGISTRY/healthcheck:latest ./HealthcheckService --push
+docker buildx build --platform linux/amd64 -t $REGISTRY/user:latest ./UserService --push
