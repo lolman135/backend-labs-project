c3958a26e4c371d674d2846deb019d46bd4376ae
Kyrylo Kusovskyi <kkirilll2006@gmail.com>
2026-04-22 22:21:32 +0300

    Change docker-build.sh to build multiarch images

--- docker-build.sh
+++ docker-build.sh
@@ -1,11 +1,15 @@
 #!/bin/zsh
 
-REGISTRY="registry.gitlab.com/kkirilll2006/devops-labs"
+docker buildx create --name multiarch-builder --use || true
+docker buildx inspect --bootstrap
 
-docker buildx build --platform linux/amd64 -t $REGISTRY/postgres:latest ./postgres --push
-docker buildx build --platform linux/amd64 -t $REGISTRY/gateway:latest ./ApiGatewayService --push
-docker buildx build --platform linux/amd64 -t $REGISTRY/auth:latest ./AuthService --push
-docker buildx build --platform linux/amd64 -t $REGISTRY/currency:latest ./CurrencyService --push
-docker buildx build --platform linux/amd64 -t $REGISTRY/expense:latest ./ExpenseService --push
-docker buildx build --platform linux/amd64 -t $REGISTRY/healthcheck:latest ./HealthcheckService --push
-docker buildx build --platform linux/amd64 -t $REGISTRY/user:latest ./UserService --push
+REGISTRY="registry.gitlab.com/kkirilll2006/devops-labs"
+PLATFORMS="linux/amd64,linux/arm64"
+
+docker buildx build --platform $PLATFORMS -t $REGISTRY/postgres:latest ./postgres --push
+docker buildx build --platform $PLATFORMS -t $REGISTRY/gateway:latest ./ApiGatewayService --push
+docker buildx build --platform $PLATFORMS -t $REGISTRY/auth:latest ./AuthService --push
+docker buildx build --platform $PLATFORMS -t $REGISTRY/currency:latest ./CurrencyService --push
+docker buildx build --platform $PLATFORMS -t $REGISTRY/expense:latest ./ExpenseService --push
+docker buildx build --platform $PLATFORMS -t $REGISTRY/healthcheck:latest ./HealthcheckService --push
+docker buildx build --platform $PLATFORMS -t $REGISTRY/user:latest ./UserService --push
